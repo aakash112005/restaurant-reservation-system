14e41c72a6656e71704489289c77c8fcae2a2e98
aakas112005 <aakashsaini112005@gmail.com>
2026-07-10 13:43:38 +0530

    addind route in app.js

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,13 @@ app.get('/api/health', (req, res) => {
   res.status(200).json({ success: true, message: 'API is healthy', time: new Date().toISOString() });
 });
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Restaurant Reservation API is running 🚀",
+  });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/reservations', reservationRoutes);
 app.use('/api/tables', tableRoutes);
